e06d08a66863b3e37f6d9534178e26b60300ad6a
Sydney Creel <sydney.creel@outlook.com>
2021-02-17 20:57:18 -0600

    install inquirer npm

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
+//imports inquirer npm
+const inquirer = require('inquirer');
+
 //require statement allows app.js file to access fs module
-const fs = require('fs');
+//const fs = require('fs');
+
 //imports page-template.js
-const generatePage = require('./src/page-template.js');
+//const generatePage = require('./src/page-template.js');
+
 //holds user command-line arguments
 const profileDataArgs = process.argv.slice(2);
+
 //array index (0 index array, "name" starts at 0)
 const [name, github] = profileDataArgs;
 
-//arguments: 1-name of file being created(output file), 2-data being written onto file(HTML string), 3-callback function used for error handling
-fs.writeFile('index.html', generatePage(name, github), err => {
-  //name property is ReferenceError. message property is "fs is not defined"
-  if (err) throw err;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-  console.log('Portfolio complete. Check out index.html to see the output.')
-});
+
+//arguments: 1-name of file being created(output file), 2-data being written onto file(HTML string), 3-callback function used for error handling
+//fs.writeFile('index.html', generatePage(name, github), err => {
+  //name property is ReferenceError. message property is "fs is not defined"
+  //if (err) throw err;
+
+  //console.log('Portfolio complete. Check out index.html to see the output.')
+//});
 
 //log inputs and confirm they match
 //console.log(name, github);
